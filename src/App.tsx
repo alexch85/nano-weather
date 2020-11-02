eeeb56d063f120d9effe52b2ff6eeb5408a90995
Alex <alexch85@windowslive.com>
2020-11-02 15:57:33 +0200

    sass version 5.0 uninstalled, sass@4.14.1 installed, themes.scss imported to App.module.scss and checked

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import styles from './App.module.scss';
 
 function App() {
-	return <div className={styles.app}></div>;
+	return (
+		<div className={styles.app}>
+			<h1>App goes here</h1>
+		</div>
+	);
 }
 
 export default App;
