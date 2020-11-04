ca592e6363b57c86a44ec02fdea1c1343a7585dd
Alex <alexch85@windowslive.com>
2020-11-04 17:44:11 +0200

    index.ts added, fetchWeather fetching weather data from openWeather api, mainDisplay component modified

---
+++ src/api/index.ts
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const APIkey: string = '8b308b3f70fca17e387bd8aab2c848e4';
+
+const url: string = `http://api.openweathermap.org/data/2.5/weather?q=ashkelon&units=metric&appid=${APIkey}`;
+
+export const fetchWeather = async () => {
+	const {
+		data: {
+			main: { temp, humidity, temp_max, temp_min, feels_like },
+			wind: { speed },
+			weather,
+			name,
+		},
+	} = await axios.get(url);
+	console.log(temp, humidity, weather);
+	const [destructuredWeather] = weather;
+
+	const { main } = destructuredWeather;
+
+	return {
+		temp,
+		humidity,
+		tempMax: temp_max,
+		tempMin: temp_min,
+		feelsLike: feels_like,
+		windSpeed: speed,
+		mainWeather: main,
+		cityName: name,
+	};
+};
--- src/components/mainDisplay/MainDisplay.tsx
+++ src/components/mainDisplay/MainDisplay.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './MainDisplay.module.scss';
 import location from '../../assets/icons/location.svg';
-import weather from '../../assets/icons/sun-cloudy.svg';
+import weatherIcon from '../../assets/icons/sun-cloudy.svg';
 import windFlag from '../../assets/icons/flag.svg';
-import humidity from '../../assets/icons/wet-1.svg';
+import humidityIcon from '../../assets/icons/wet-1.svg';
 import options from '../../assets/icons/options.svg';
+import { fetchWeather } from '../../api/index';
+
+interface todayWeather {
+	temp: number;
+	humidity: number;
+	tempMin: number;
+	tempMax: number;
+	feelsLike: number;
+	windSpeed: number;
+	mainWeather: string;
+	cityName: string;
+}
 
 const MainDisplay: React.FC = () => {
+	const [todayWeather, setTodayWeather] = useState<todayWeather>({
+		temp: 0,
+		humidity: 0,
+		tempMax: 0,
+		tempMin: 0,
+		feelsLike: 0,
+		windSpeed: 0,
+		mainWeather: '',
+		cityName: '',
+	});
+
+	useEffect(() => {
+		const fetchAPI = async () => {
+			setTodayWeather(await fetchWeather());
+		};
+		fetchAPI();
+	}, []);
+
+	const { temp, humidity, feelsLike, windSpeed, mainWeather, cityName } = todayWeather;
+
 	return (
 		<div
 			className={styles.mainDisplayContainer}
@@ -18,26 +50,28 @@ const MainDisplay: React.FC = () => {
 			<div className={styles.dateContainer}>{new Date().toDateString()}</div>
 			<div className={styles.locationContainer}>
 				<img alt='location' src={location} height='15px' />
-				Madrid
+				{cityName}
 			</div>
-			<div className={styles.tempContainer}>21°</div>
+			<div className={styles.tempContainer}>{Math.round(temp)}°</div>
+
 			<div className={styles.weatherContainer}>
-				<img alt='weather' src={weather} height='25px' />
-				cloudy
+				<img alt='weather' src={weatherIcon} height='25px' />
+				{mainWeather}
 			</div>
+			<div>Feels like {Math.round(feelsLike)}°</div>
 			<div className={styles.sideContainer}>
 				<div className={styles.humidContainer}>
 					<img
 						alt='humidity'
-						src={humidity}
+						src={humidityIcon}
 						height='15px'
 						style={{ marginRight: '5px', marginLeft: '-2px' }}
 					/>
-					52%
+					{humidity}%
 				</div>
 				<div className={styles.windContainer}>
 					<img alt='wind' src={windFlag} height='12px' />
-					North-west 5 meters per hour
+					North-west {windSpeed} meters per hour
 				</div>
 			</div>
 		</div>
