8f78fbfc3c0ce87e60f2a8a4fec3eaaf4c4eae0c
Alex <alexch85@windowslive.com>
2020-11-09 19:32:36 +0200

    bgWrapper useState ,useEffect and handler added for backgrounds control, styles in searchBox modified

--- src/components/UI/bgWrapper/BgWrapper.tsx
+++ src/components/UI/bgWrapper/BgWrapper.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { wrapperPropsI } from '../../../interfaces';
 import styles from './BgWrapper.module.scss';
 
-const bgWrapper: React.FC<wrapperPropsI> = ({ children }) => {
-	const date = new Date();
-	const time = date.getHours();
+const BgWrapper: React.FC<wrapperPropsI> = ({ children }) => {
+	const [background, setBackground] = useState('');
+	const setBackgroundHandler = () => {
+		const date = new Date();
+		const time = date.getHours();
 
-	let background = '';
-	if (time > 6 && time < 12) {
-		background = 'url(backgrounds/bg-mobile-clear-dawn@2x.jpg';
-	}
-	if (time > 12 && time < 17) {
-		background = 'url(backgrounds/bg-mobile-clear-noon@2x.jpg';
-	}
-	if (time > 17 && time < 20) {
-		background = 'url(backgrounds/bg-mobile-clear-dusk@2x.jpg';
-	}
-	if (time > 20 || time < 6) {
-		background = 'url(backgrounds/bg-mobile-clear-night@2x.jpg';
-	}
+		let background = '';
+
+		if (time > 6 && time < 12) {
+			background = 'url(backgrounds/bg-mobile-clear-dawn@2x.jpg)';
+		}
+		if (time > 12 && time < 17) {
+			background = 'url(backgrounds/bg-mobile-clear-noon@2x.jpg)';
+		}
+		if (time > 17 && time < 20) {
+			background = 'url(backgrounds/bg-mobile-clear-dusk@2x.jpg)';
+		}
+		if (time > 20 || time < 6) {
+			background = 'url(backgrounds/bg-mobile-clear-night@2x.jpg)';
+		}
+		return background;
+	};
+	useEffect(() => {
+		setBackground(setBackgroundHandler());
+	}, []);
 
 	return (
 		<div className={styles.bgWrapper} style={{ backgroundImage: background }}>
@@ -27,4 +35,4 @@ const bgWrapper: React.FC<wrapperPropsI> = ({ children }) => {
 	);
 };
 
-export default bgWrapper;
+export default BgWrapper;
