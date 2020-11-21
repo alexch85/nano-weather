b5f4d50cd614380755e4ece7e98d7d65fe2b9a7e
Alex <alexch85@windowslive.com>
2020-11-21 20:14:08 +0200

    DailyWeather component modified

--- src/components/dailyWeatherDisplay/dailyWeather/DailyWeather.tsx
+++ src/components/dailyWeatherDisplay/dailyWeather/DailyWeather.tsx
@@ -4,10 +4,9 @@ import WeatherIcon from '../../UI/weatherIcon/WeatherIcon';
 import styles from './DailyWeather.module.scss';
 
 const WeatherDay: React.FC<IDailyWeatherProps> = ({ date, weather, tempMin, tempMax }) => {
-	var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-	var d = new Date(date * 1000);
-	var dayName = days[d.getDay()];
-	console.log(dayName);
+	const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+	const day = new Date(date * 1000);
+	const dayName = days[day.getDay()];
 	return (
 		<div className={styles.weatherDay}>
 			<div>{dayName}</div>
